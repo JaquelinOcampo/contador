a2f1735e4255bd17634aecb9b408e9d51060b74c
JaquelinOcampo <132309793+JaquelinOcampo@users.noreply.github.com>
2023-05-13 14:42:31 -0500

    calculadora2

--- src/app/componentes/calculadora/calculadora.component.ts
+++ src/app/componentes/calculadora/calculadora.component.ts
@@ -6,18 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent implements OnInit {
-  public formula:string="";
-public Resultado:string="";
+  public formula: string = "";
+  public Resultado: string = "coloque la formula";
   constructor() { }
 
   ngOnInit(): void {
   }
-calcular(){
-  let x=eval(this.formula);
-  this.Resultado=this.formula+"="+x;
-  this.formula="";
-}
-retroceso (){
-  this.formula=this.formula.substr(0,this.formula.length-1);
-}
+  calcular() {
+    let x = eval(this.formula);
+    this.Resultado = this.formula + "=" + x;
+    this.formula = "";
+  }
+  retroceso() {
+    this.formula = this.formula.substr(0, this.formula.length - 1);
+  }
+  limpiar() {
+    this.formula="";
+    this.Resultado="coloque la formula";
+  }
+
+
 }
