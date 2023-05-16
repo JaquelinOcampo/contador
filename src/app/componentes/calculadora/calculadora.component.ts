84d3dcc264b4036ef1589e5f5f4ed2b21c6e5b1f
JaquelinOcampo <132309793+JaquelinOcampo@users.noreply.github.com>
2023-05-15 21:19:50 -0500

    calculadora basica

--- src/app/componentes/calculadora/calculadora.component.ts
+++ src/app/componentes/calculadora/calculadora.component.ts
@@ -8,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class CalculadoraComponent implements OnInit {
   public formula: string = "";
   public Resultado: string = "coloque la formula";
+  public display: string="0";
+  private resultado="";
+  private formular="";
+  public error:boolean =false;
   constructor() { }
 
   ngOnInit(): void {
+// calcoladora
+  }
+  mostrar(valor: string){
+    if(this.resultado!=""){
+      this.resultado="",
+      this.display=this.formular;
+    }
+    if(this.display=="0"){
+      this.display="";
+    }
+    this.display=this.display+valor;
   }
   calcular() {
     let x = eval(this.formula);
@@ -24,6 +39,30 @@ export class CalculadoraComponent implements OnInit {
     this.formula="";
     this.Resultado="coloque la formula";
   }
-
-
+  // calcoladora
+AC(){
+  this.display="0";
+  this.resultado="";
+  this.formular="";
 }
+C(){
+  if(this.formular.length>0){
+    this.display=this.formular;
+    this.resultado="";
+    this.formular="";
+  }
+  this.display=this.display.substr(0,this.display.length-1)
+  this.display=(this.display=="")?"0":this.display;
+}
+calc(){
+  this.formular=this.display;
+  try {
+    this.resultado=eval(this.display);
+  }catch {
+    this.resultado="error"
+    this.error=true;
+  }
+  this.display=this.formular+"\n"+this.resultado;
+}
+}
+
